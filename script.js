5730c4b038e2185f94e043404e1bd2eaf80e04ff
Jawad Malik <jmalik94@outlook.com>
2023-10-26 14:01:40 +0100

    cell background color changes upon hover

--- script.js
+++ script.js
@@ -5,7 +5,23 @@ const createGrid = (gridSize) => {
         const gridCell = document.createElement('div');
         gridCell.classList.add('grid-cell');
         gridContainer.appendChild(gridCell);
+
+        gridCell.addEventListener('mouseenter', () => {
+            gridCell.style.backgroundColor = 'yellow';
+        })
+
+        gridCell.addEventListener('mouseleave', () => {
+            gridCell.style.backgroundColor = 'red';
+        })
     }
 }
 
 createGrid(16);
+
+gridCell.addEventListener('mouseenter', () => {
+    gridCell.style.backgroundColor = 'yellow';
+})
+
+gridCell.addEventListener('mouseleave', () => {
+    gridCell.style.backgroundColor = 'red';
+})
