307820db9b4d42507bb8222850a2f64ff1295e4e
Jawad Malik <jmalik94@outlook.com>
2023-10-26 14:06:18 +0100

    pixelate function defined to add/remove class to change background color upon hover for improved readability

--- script.js
+++ script.js
@@ -6,22 +6,18 @@ const createGrid = (gridSize) => {
         gridCell.classList.add('grid-cell');
         gridContainer.appendChild(gridCell);
 
-        gridCell.addEventListener('mouseenter', () => {
-            gridCell.classList.add('grid-cell-active')
-        })
-
-        gridCell.addEventListener('mouseleave', () => {
-            gridCell.classList.remove('grid-cell-active');
-        })
+        pixelate(gridCell);
     }
 }
 
+const pixelate = (cell) => {
+    cell.addEventListener('mouseenter', () => {
+        cell.classList.add('grid-cell-active')
+    })
+
+    cell.addEventListener('mouseleave', () => {
+        cell.classList.remove('grid-cell-active');
+    })
+}
+
 createGrid(16);
-
-gridCell.addEventListener('mouseenter', () => {
-    gridCell.style.backgroundColor = 'yellow';
-})
-
-gridCell.addEventListener('mouseleave', () => {
-    gridCell.style.backgroundColor = 'red';
-})
