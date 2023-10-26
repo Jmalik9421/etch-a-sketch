ca651cc3c4c0ab32cb651d58046fd23b47a02d20
Jawad Malik <jmalik94@outlook.com>
2023-10-26 13:49:41 +0100

    16x16 grid initalised

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('hello world');
+const gridContainer = document.getElementById('grid-container');
+
+const createGrid = (gridSize) => {
+    for (let i = 0; i < gridSize ** 2; i++) {
+        const gridCell = document.createElement('div');
+        gridCell.classList.add('grid-cell');
+        gridContainer.appendChild(gridCell);
+    }
+}
+
+createGrid(16);
